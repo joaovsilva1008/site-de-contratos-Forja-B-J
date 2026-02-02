5a4b324522d1370dfe5b46381ae3fc351ab7fbc9
jvzin <joao.v.silva1008@aluno.senai.br>
2026-02-02 17:11:28 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 function emprE(){
-    
+    const nome = document.getElementById('nome').value
+    const assunto = document.getElementById('assunto').value
+    const mensagem = ` Gostaria de entrar em contato!\n\nNome: ${nome} \nAssunto: ${assunto}`
+    const msg = encodeURIComponent(mensagem)
+    window.open(`https://wa.me/55419********?text=${msg}`)
+
 }
